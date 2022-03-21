2756b55bd9c775ae400d3cb0f0c2c7eedcf141b3
kumpon J Sotsukpiam <kumpon_s@brainergy.digital>
2022-03-22 01:58:26 +0700

    fix: Type error: Type 'NextRequest' is not assignable to type 'NextApiRequest | Pick<NextApiRequest, headers | cookies>'.

--- pages/_middleware.tsx
+++ pages/_middleware.tsx
@@ -2,11 +2,11 @@ import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest | any) {
     const token = await getToken({
         req: request, secret: process.env.JWT_SECRET
     });
-    
+
     const { pathname } = request.nextUrl;
     if (pathname.includes('/api/auth') || token) {
         return NextResponse.next();
